347b6fe1e7144a74a58f64062dad45a582179885
Ayodele Opetumo <aopetumo@tripactions.com>
2022-02-10 02:28:43 +0100

    ZP-000 create shell component for creating posts

---
+++ src/app/posts/create-post/create-post.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: 'create-post.component.html'
+})
+
+export class CreatePostComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
