f44b91a966f08825082d06713490d319d5d693e8
Ayodele Opetumo <aopetumo@tripactions.com>
2022-03-06 22:18:55 +0100

    ZS-002 add create posts template (#3)

---
+++ src/app/features/features.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+// Modules
+import { PostsModule } from './posts/posts.module';
+
+@NgModule({
+  imports: [PostsModule],
+  exports: [PostsModule],
+  declarations: [],
+  providers: [],
+})
+export class FeaturesModule { }
---
+++ src/app/ui/components/header/ui.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Components
+import { HeaderComponent } from './header.component';
+
+
+@NgModule({
+  imports: [
+    MatToolbarModule
+  ],
+  exports: [HeaderComponent],
+  declarations: [HeaderComponent],
+  providers: [],
+})
+export class UIModule { }
