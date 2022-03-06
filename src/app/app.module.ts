811670f815538297ae700fdc9c9fb10f9226b0c9
Ayodele Opetumo <aopetumo@gmail.com>
2022-03-06 22:18:55 +0100

    ZS-002 add create posts template (#3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { AppRoutingModule } from './app-routing.module';
+import { UIModule } from './ui/components/header/ui.module';
 
 import { AppComponent } from './app.component';
-import { CreatePostComponent } from './posts/create-post/create-post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreatePostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    UIModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
