ed4dd8118b0bf336586b73470f57de2ad229e72a
Ayodele Opetumo <aopetumo@tripactions.com>
2022-03-09 01:41:19 +0100

    ZS-005 add Node BE to project (#6)

--- src/app/features/posts/posts.service.ts
+++ src/app/features/posts/posts.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
 
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
-  
-  constructor() { }
+  private postsUpdated = new Subject<Post[]>()
 
-  getPosts() {}
+  constructor(private http: HttpClient) {}
 
-  addPost() {}
+  getPosts() {
+    this.http.get<{message: string; posts: Post[]}>('http://localhost:3000/api/posts').subscribe((data) => {
+      this.posts = data.posts;
+      this.postsUpdated.next([...this.posts])
+    });
+  }
+
+  getPostUpdateListener(){
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(post: Post) {
+    const payload = {...post, id: null};
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', payload).subscribe((data) => {
+      console.log(data);
+      this.posts.push(payload);
+      this.postsUpdated.next([...this.posts]);
+    })
+  }
 }
