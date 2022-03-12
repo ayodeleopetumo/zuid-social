4f65b57f78a4745a069f7afe216a69d87e008494
Ayodele Opetumo <aopetumo@gmail.com>
2022-03-12 03:34:12 +0100

    ZS-007 add enhancements across the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListPostComponent } from "./features/posts/list-post/list-post.component";
+import { CreatePostComponent } from "./features/posts/create-post/create-post.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListPostComponent
+  },
+  {
+    path: 'create',
+    component: CreatePostComponent
+  },
+  {
+    path: 'edit/:postId',
+    component: CreatePostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/features/posts/list-post/list-post.component.ts
+++ src/app/features/posts/list-post/list-post.component.ts
@@ -9,15 +9,20 @@ import { Post } from '../post.model';
   styleUrls: ['list-post.component.scss'],
 })
 export class ListPostComponent implements OnInit {
+  isLoading = false;
   posts: Post[] = [];
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.postsService.getPosts();
     this.postsService
       .getPostUpdateListener()
-      .subscribe((posts) => (this.posts = posts));
+      .subscribe((posts) => {
+        this.isLoading = false
+        this.posts = posts
+      });
   }
 
   onPostDelete(postId: string) {
